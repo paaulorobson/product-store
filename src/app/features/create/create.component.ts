5c9ff63098a4312801f89360693fb7bc3bc99b40
Paulo Robson <p.robson085@gmail.com>
2024-06-19 20:20:23 -0300

    feat: adiciona snackbar

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -8,7 +8,9 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -24,6 +26,9 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class CreateComponent {
   productService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+
   form = new FormGroup({
     title: new FormControl<string>('', {
       nonNullable: true,
@@ -37,7 +42,12 @@ export class CreateComponent {
         title: this.form.controls.title.value,
       })
       .subscribe(() => {
-        alert('201');
+        this.matSnackBar.open('Produto criado com sucesso!', 'OK', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+        this.router.navigateByUrl('/');
       });
   }
 }
